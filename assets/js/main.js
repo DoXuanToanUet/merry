1f29b59e2a8a5e840b5a306a7cdcf1796dd39335
elumarketing <marketing@elightup.com>
2023-12-21 16:38:46 +0700

    update background and layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 person = [
   {
     pass: "nht",
@@ -175,3 +177,7 @@ $(document).ready(function () {
     $(".popup").toggle();
   });
 });
+
+fetch( 'http://127.0.0.1:5500/assets/js/data.json' )
+  .then( ( response ) => response.json() )
+  .then( ( json ) => console.log( json ) );
